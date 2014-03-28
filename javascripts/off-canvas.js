3d78246c3b12e41e89cc0b016792f0a55c1d558d
shengli1989 <shengli@shenglideMacBook-Pro.local>
2014-03-28 13:23:59 +0800

    off-canvas

--- javascripts/off-canvas.js
+++ javascripts/off-canvas.js
@@ -1,12 +1,7 @@
 var off_canvas = function (){
 
-  $(".off-canvas-toggle-btn").on("click", function(event){
+  $(".off-canvas-toggle-btn, .main-nav a").on("click", function(event){
     $("body").toggleClass("open-menu");
-    event.preventDefault;
-  });
-
-  $(".outside a, .off-canvas-mask").on("click", function(){
-    $("body").removeClass("open-menu");
   });
 };
 
