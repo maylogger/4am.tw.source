47886aa5db3a62c9d902c91f910598a762902b4d
Even <evenwu@users.noreply.github.com>
2023-06-23 21:41:57 +0800

    update

--- docs/javascripts/off-canvas.js
+++
@@ -1,9 +0,0 @@
-var off_canvas = function (){
-
-  $(".toggle-menu-btn, .story").on("click", function(event){
-    $("body").toggleClass("open-menu");
-    event.preventDefault();
-  });
-};
-
-off_canvas();
