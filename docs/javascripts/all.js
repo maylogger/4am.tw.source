6e869df73ddad94bc7f3d49649bcca3b2f307c27
maylogger <vtuber@maylogger.com>
2024-05-19 20:56:57 +0800

    zhtw update

---
+++ docs/javascripts/all.js
@@ -0,0 +1,12 @@
+// 移動 dom
+// $( ".main-nav ul" ).appendAround();
+
+// scrollable
+var scrollable = document.getElementById("scrollable");
+new ScrollFix(scrollable);
+
+// image lazyload
+// $("img.lazy").lazyload();
+$("img.lazy").lazyload({
+  effect: "fadeIn",
+});
